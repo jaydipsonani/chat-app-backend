9f3d488b2f537d96212de997323aba3cd2a65d65
Jaydip Sonani <jaydip.sonani@cloudusinfotech.com>
2024-08-21 17:17:14 +0530

    chat-app

--- config/db.js
+++ config/db.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/chat', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('MongoDB connection failed:', err.message);
-        process.exit(1);
-    }
+  // try {
+  //     await mongoose.connect('mongodb://localhost:127.0.0.1:27017/chat', {
+  //         useNewUrlParser: true,
+  //         useUnifiedTopology: true,
+  //     });
+  //     console.log('MongoDB connected');
+  // } catch (err) {
+  //     console.error('MongoDB connection failed:', err.message);
+  //     process.exit(1);
+  // }
+ await mongoose.connect("mongodb://127.0.0.1:27017/chat")
+    .then(() => console.log("Connected!"));
 };
 
 module.exports = connectDB;
-
