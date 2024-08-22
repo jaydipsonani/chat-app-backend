3e3597f112fdaecb808d7474ed78b8ec3f2a7a6c
Jaydip Sonani <jaydip.sonani@cloudusinfotech.com>
2024-08-22 18:24:42 +0530

    vercel-update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", 
+        origin: "https://chat-app-fronted-seven.vercel.app/", 
         methods: ["GET", "POST"],
     },
 });
