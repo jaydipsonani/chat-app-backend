f71f169973680f44adc5596da3c0fe3790380c25
jaydipsonani <132323340+jaydipsonani@users.noreply.github.com>
2024-09-10 10:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", 
+        origin: "https://chat-app-fronted-zeta.vercel.app", 
         methods: ["GET", "POST"],
     },
 });
